f94270701a0fb837c66dadc734fb246766e5af0c
Will Jackson <william.b.jackson.14@dartmouth.edu>
2012-05-08 11:42:50 -0400

    Added definition

---
+++ src/operator/operator.h
@@ -0,0 +1,29 @@
+// Science Fiction defs
+
+/*
+
+1. Suvan
+
+2. Setting vs. Relations
+
+3. Logic in relation to world vs. in relation to itself
+
+4. Phasers vs. Swords
+
+5. What if - technological
+
+6. Symbolism in realist style
+
+7. Novum - effort to explain change
+
+8. Believable impossibilities vs. improbable possibilities
+	Could possibly come true
+	Miriam Allen DuFord
+
+9. Philosophical masturbation
+
+*/
+
+// Dan Rockmore (Not Ellen)
+
+// Picaresque
