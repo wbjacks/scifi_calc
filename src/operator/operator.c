b4390a1b6a74ac03b7d51789c27cee11e320e465
Will Jackson <william.b.jackson.14@dartmouth.edu>
2012-05-29 03:56:23 -0500

    Added most of interface as well as NETWORK type

--- src/operator/operator.c
+++ src/operator/operator.c
@@ -19,13 +19,13 @@ int main(int argc, char *argv[]){
 	printf("\t[E]xit\n\n");
 	printf("Selection: ");
 
-	scanf(input);
+	scanf("%s\n", input);
 	
 	switch (input[0]) {
-		case 'l': loadMatrixThenSubMenu();
-		case 'c': createMatrixThenMainMenu();
+		case 'c': createMatrixThenMainMenu(); return 0;
+		case 'l': loadMatrixThenSubMenu(); return 0;
 		case 'e': return 0;
-		default: fprintf(stderr, "Error: Improper input"); main();
+		default: fprintf(stderr, "Error: Improper input\n"); main();
 	
 	}
 	
@@ -63,5 +63,69 @@ int main(int argc, char *argv[]){
 	printMatrix(*wgt);
 
 */
+
+}
+
+void loadMatrixThenSubMenu() {
+	
+	char input[MAX_FILE_NAME_LENGTH];
+	FILE *iFile;
+	NETWORK *n;
+	
+	system("clear");
+	printf("Please input filename to load or [ENTER] to exit: ");
+	scanf("%s\n", input);
+	
+	if (!input){
+		return;
+	
+	}
+	
+	n = dataLoad(input);
+	
+	subMenu(n);
+	return;
+
+}
+
+void createMatrixThenSubMenu {
+
+	int i;
+	int val;
+	char *input;
+	FILE *oFile;
+	NETWORK *n;
+		n = malloc(sizeof(NETWORK));
+			memset(n, 0, sizeof(NETWORK));
+		n->weights = malloc(sizeof(MATRIX));
+	
+	system("clear");
+	
+	printf("NOTE: Maximum of 20 queries.\n");
+	for(i = 0; i < 20; i++) {
+		printf("\nPlease input query: ");
+		memset(input, 0, MAX_QUERY_LENGTH);
+		input = malloc(MAX_QUERY_LENGTH);
+		scanf("%s\n", input);
+		n->queries[i] = input;
+	
+	}
+	
+	n->weights->rows = i;
+	n->weights->cols = 1;
+	n->weights->values = malloc(n->weights->rows);
+	printf("Please input initial value of weight matrix: ");
+	scanf("%d\n", &val);
+	memset(n->weights->values, val, n->weights->rows);
+	
+	subMenu(n);
+	return;
+
+}
+
+void subMenu(NETWORK *n) {
+
+	system("clear");
+	printf("Shit!\n");
 
 }
--- src/operator/operator.h
+++ src/operator/operator.h
@@ -1,31 +1,11 @@
-// Science Fiction defs
-
-/*
-
-1. Suvan
-
-2. Setting vs. Relations
-
-3. Logic in relation to world vs. in relation to itself
-
-4. Phasers vs. Swords
-
-5. What if - technological
-
-6. Symbolism in realist style
-
-7. Novum - effort to explain change
-
-8. Believable impossibilities vs. improbable possibilities
-	Could possibly come true
-	Miriam Allen DuFord
-
-9. Philosophical masturbation
-
-*/
-
-// Dan Rockmore (Not Ellen)
-
-// Picaresque
-
 #define MAX_INPUT_LENGTH 15
+#define MAX_NUMBER_OF_QUERIES 20
+#define MAX_QUERY_LENGTH 30
+
+struct _network {
+	char *queries[MAX_NUMBER_OF_QUERIES];
+	struct _matrix *weights;
+
+};
+
+typedef _network NETWORK;
