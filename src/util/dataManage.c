9a00a4af350e159794ce312d8712eb5e4b8638dc
Will Jackson <william.b.jackson.14@dartmouth.edu>
2012-05-29 04:08:32 -0500

    Added (very buggy) save functionality

--- src/util/dataManage.c
+++ src/util/dataManage.c
@@ -1,18 +1,90 @@
-int dataSaveMatrix(char *mat_file, MATRIX *save_matrix){
+#include <stdio.h>
+#include <stdlib.h>
+#include "dataManage.h"
+#include "matrix.h"
+#include "../operator/operator.h"
+
+int dataSave(struct _network *n){
 
 	FILE *oFile;
 	char *formatted_matrix;
+	char *formatted_queries;
 	char data_dir[] = {"../data/"}
-	char *data_path;
+	char filename_matrix[MAX_FILE_NAME];
+		memset(filename_matrix, 0, MAX_FILE_NAME);
+	char filename_queries[MAX_FILE_NAME];
+		memset(filename_queries, 0, MAX_FILE_NAME);
+	char *data_path_matrix;
+	char *data_path_queries;
 	
-	data_path = strcat(mat_file, data_dir);
-	formatted_matrix = formatMatrixForSave(save_matrix);
+	// Grab matrix file
+	printf("\nPlease input name of file to save matrix: ");
+	scanf("%s\n", filename);
+	data_path_matrix = strcat(filename_matrix, data_dir);
+	
+	// Format output
+	formatted_matrix = formatMatrixForSave(n->weights);
+	formatted_queries = formatQueriesForSave(n->queries);
 
-	oFile = fopen(data_path, "w");
+	// Print matrix file
+	oFile = fopen(data_path_matrix, "w");
 	fprintf(oFile, formatted_matrix);
 	fclose(oFile);
 	free(formatted_matrix);
 	
+	// Grab query file
+	printf("\nPlease input name of file to save queries: ");
+	scanf("%s\n", filename_queries);
+	data_path_queries = strcat(filename_queries, data_dir);
+	
+	// Print query file
+	oFile = fopen(data_path_queries, "w");
+	fprintf(oFile, formatted_queries);
+	fclose(oFile);
+	free(formatted_queries);
+	
 	return 1;
 
 }
+
+char *formatMatrixForSave(struct _matrix *m) {
+
+	int i;
+	char delim[] = {"!*@#$"};
+	char *s;
+		s = malloc(MAX_SIZE_OF_MATRIX_ENCRYPTION);
+		memset(s, 0, MAX_SIZE_OF_MATRIX_ENCRYPTION);
+	
+	sprintf(s, "%c%s", m->rows, delim);
+	s = strcat(s, n->cols);
+	
+	for(i = 0; i < (m->rows * m->cols); i++) {
+		s = strcat(s, delim);
+		s = strcat(s, m->values[i]);
+	
+	}
+	
+	return s;
+
+}
+
+char *formatQueriesForSave(char *qs[]) {
+
+	int i;
+	char *out;
+		out = malloc(MAX_SIZE_OF_QUERY_ENCRYPTION);
+		memset(s, 0, MAX_SIZE_OF_QUERY_ENCRYPTION);
+	char delim[] = "\n";
+
+	sprintf(out, "%s", query);
+	
+	for (i = 1; qs[i]; i++) {
+		out = strcat(out, delim);
+		out = strcat(out, qs[i]);
+		
+	}
+	
+	return out;
+
+}
+
--- src/util/dataManage.h
+++ src/util/dataManage.h
@@ -1,6 +1,10 @@
 //dataManage.h
 
-#define MAX_FILENAME_SIZE 20
+#define MAX_FILENAME 20
+#define MAX_SIZE_OF_MATRIX_ENCRYPTION 100000
+#define MAX_SIZE_OF_QUERY_ENCRYPTION 3000000
 
-int dataSave();
-int dataLoad();
+int dataSave(struct _network *n)
+char *formatQueriesForSave(char *qs[]);
+char *formatMatrixForSave(struct _matrix *m);
+
